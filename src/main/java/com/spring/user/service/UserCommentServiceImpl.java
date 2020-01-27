5f1257b5b8c73e15a149203a9955eb3722733e4b
murat yuzkollar <mrtyuz@gmail.com>
2020-01-27 21:33:20 +0300

    rest calls implemented by async

--- src/main/java/com/spring/user/service/UserCommentServiceImpl.java
+++ src/main/java/com/spring/user/service/UserCommentServiceImpl.java
@@ -3,10 +3,13 @@ package com.spring.user.service;
 
 import com.spring.user.model.UserCommentDto;
 import org.springframework.boot.web.client.RestTemplateBuilder;
+import org.springframework.scheduling.annotation.Async;
+import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 
 import java.util.concurrent.CompletableFuture;
 
+@Service
 public class UserCommentServiceImpl implements UserCommentService {
     private final RestTemplate restTemplate;
 
@@ -16,6 +19,7 @@ public class UserCommentServiceImpl implements UserCommentService {
     }
 
     @Override
+    @Async
     public CompletableFuture<UserCommentDto[]> findUserComment(Long userId) {
         String url = String.format("http://jsonplaceholder.typicode.com/posts?userId=%s", userId);
         return CompletableFuture.completedFuture(restTemplate.getForObject(url, UserCommentDto[].class));
--- src/main/java/com/spring/user/service/UserServiceImpl.java
+++ src/main/java/com/spring/user/service/UserServiceImpl.java
@@ -4,11 +4,14 @@ package com.spring.user.service;
 import com.spring.user.exception.EntityNotFoundException;
 import com.spring.user.model.UserDto;
 import org.springframework.boot.web.client.RestTemplateBuilder;
+import org.springframework.scheduling.annotation.Async;
+import org.springframework.stereotype.Service;
 import org.springframework.web.client.HttpStatusCodeException;
 import org.springframework.web.client.RestTemplate;
 
 import java.util.concurrent.CompletableFuture;
 
+@Service
 public class UserServiceImpl implements UserService {
     private final RestTemplate restTemplate;
 
@@ -18,6 +21,7 @@ public class UserServiceImpl implements UserService {
 
 
     @Override
+    @Async
     public CompletableFuture<UserDto> findUser(Long userId) {
         String url = String.format("http://jsonplaceholder.typicode.com/users/%s", userId);
         UserDto userDto;
