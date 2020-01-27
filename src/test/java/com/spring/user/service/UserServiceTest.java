9948f5ddfacc424df52472c05c98cacaebe3c656
murat yuzkollar <mrtyuz@gmail.com>
2020-01-27 17:01:19 +0300

    initial commit

---
+++ src/test/java/com/spring/user/service/UserServiceTest.java
@@ -0,0 +1,11 @@
+package com.spring.user.service;
+
+
+import org.junit.jupiter.api.Test;
+
+public class UserServiceTest {
+    @Test
+    public void createUserServiceTestObject() {
+        UserService userService = new UserService();
+    }
+}
